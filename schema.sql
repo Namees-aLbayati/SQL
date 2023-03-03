9b874e33bf13241eccb3eeca0d99b5ff9eebd31d
Namees Albayati <nameesalbayati@fb.com>
2023-03-03 14:23:34 -0600

    sql quiries

---
+++ schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS mydb;
+CREATE DATABASE mydb;
+USE mydb;
+
+DROP TABLE 'employee';
+CREATE TABLE employee (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+
+);
+id as int, fname, lname, age as int, hiredate as date.
+
